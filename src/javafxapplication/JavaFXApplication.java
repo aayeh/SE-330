85d408bee173acb608156deb2c594f607b4bd97f
Aaron Yeh <44712755+aayeh@users.noreply.github.com>
2021-09-20 20:36:11 -0700

    Update JavaFXApplication.java

--- src/javafxapplication/JavaFXApplication.java
+++ src/javafxapplication/JavaFXApplication.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javafxapplication; d01
+package javafxapplication;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
