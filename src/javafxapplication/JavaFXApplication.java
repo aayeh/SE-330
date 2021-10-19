ab3530ee167b51cf7a398ac729d9b5072b4a013f
Aaron Yeh <44712755+aayeh@users.noreply.github.com>
2021-10-18 17:43:12 -0700

    Update JavaFXApplication.java

--- src/javafxapplication/JavaFXApplication.java
+++ src/javafxapplication/JavaFXApplication.java
@@ -22,7 +22,7 @@ public class JavaFXApplication extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
-        btn.setText("Say 'Hello World'");
+        btn.setText("Say 'Hello World!'");
         btn.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
