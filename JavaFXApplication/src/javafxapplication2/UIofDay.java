d1ad0d55af5770764e35771a20fb6655f706689e
aayeh <aayeh@chapman.edu>
2021-11-01 16:43:04 -0700

    Updating files and adding new ones

---
+++ JavaFXApplication/src/javafxapplication2/UIofDay.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafxapplication2;
+
+/**
+ *
+ * @author aaronyeh
+ * this is where ill create different buttons after clicking on a certain day
+ */
+public class UIofDay {
+    String day;
+    public String getDay(){
+        return day;
+    }
+    public void setDay(String day){
+        this.day = day;
+    }
+    
+}
