3074d6366a02fbd1b70b4e9743c75fcfa9d8e67e
aayeh <aayeh@chapman.edu>
2021-11-15 19:01:51 -0800

    adding files

--- JavaFXApplication/src/javafxapplication2/JavaFXApplication2.java
+++ JavaFXApplication/src/javafxapplication2/JavaFXApplication2.java
@@ -1,79 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
- */
+package javafxapplication2;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-
-/**
- * Main class
- * Currently a template making a grid style calendar
- * Will probably change this to a 4x3 grid of months and have user choose what month to click into
- * @author aaronyeh
- */
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+ 
 public class JavaFXApplication2 extends Application {
-    
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("GridPane Experiment");
-        
-        GridPane gridPane = new GridPane();
-        
-        for(int i = 1; i < 32; i++){
-
-            String s = String.valueOf(i);
-            Button createButton = new Button(s);
-            if(i <= 7){
-               gridPane.add(createButton, i, 0, 1, 1);
-               createButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-            }
-            else if(i <= 14){
-               int n = i - 7;
-               gridPane.add(createButton, n, 1, 1, 1);
-               createButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-               
-                
-            }
-            else if(i <= 21){
-               int n = i - 14;                
-               gridPane.add(createButton, n, 2, 1, 1); 
-               createButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-                
-            }
-            else if(i <= 28){
-               int n = i - 21;                                
-               gridPane.add(createButton, n, 3, 1, 1); 
-               createButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-                
-            }
-            else{
-               int n = i - 28;                                                
-               gridPane.add(createButton, n, 4, 1, 1);    
-               createButton.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-               
-            }
-        }
-        gridPane.setHgap(200);
-        gridPane.setVgap(200);
-        primaryStage.setMaximized(true);
-        Scene scene = new Scene(gridPane, 300, 150);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-    }
-
-    /**
-     * @param args the command line arguments
-     */
+ 
+    private Stage stage;
+    private DatePicker checkInDatePicker;
     public static void main(String[] args) {
         launch(args);
     }
-    
+ 
+    @Override
+    public void start(Stage stage) {
+        this.stage = stage;
+        stage.setTitle("Calendar ");
+        initUI();
+        stage.show();
+    }
+ 
+    private void initUI() {
+        VBox vbox = new VBox(20);
+        vbox.setStyle("-fx-padding: 10;");
+        Scene scene = new Scene(vbox, 400, 400);
+        stage.setMaximized(true);
+        stage.setScene(scene);
+
+        checkInDatePicker = new DatePicker();
+
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(20);
+        gridPane.setVgap(20);
+
+        Label checkInlabel = new Label("Select a date");
+        gridPane.add(checkInlabel, 0, 0);
+
+        GridPane.setHalignment(checkInlabel, HPos.LEFT);
+        gridPane.add(checkInDatePicker, 0, 1);
+        vbox.getChildren().add(gridPane);
+    }
 }
